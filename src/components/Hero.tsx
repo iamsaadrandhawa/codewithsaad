55cd54c29b7a70c20acfb3e28bd40712aa1f4f50
Saad Randhawa <139648238+iamsaadrandhawa@users.noreply.github.com>
2025-01-07 23:34:20 +0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20">
         <div className="flex flex-col md:flex-row items-center justify-between">
           <div className="md:w-1/2 text-center md:text-left">
-            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+           <h1 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-5xl">
               <span className="block">Hi, I'm Saad</span>
               <span className="block h-15"><TypeWriter words={titles} /></span>
             </h1>
